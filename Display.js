ce276690e7c441b2f9b9473a8a202ef4c13c6aa5
ROHIT KUMAR <rohitksingh887@gmail.com>
2023-07-17 15:32:18 +0530

    Color Change

---
+++ Display.js
@@ -0,0 +1,37 @@
+// Gernate Random Color
+
+const randomColor = function ()
+{
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0 ; i < 6; i++)
+    {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+let interval ;
+
+const startChangingColor = function ()
+{
+    interval = setInterval(changeBgColor , 1000);
+    function  changeBgColor()
+    {
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+
+const  stopChangingColor = function () {
+
+    clearInterval(interval);
+    interval = null;
+}
+
+document.querySelector("#start").
+addEventListener('click' ,startChangingColor)
+
+
+document.querySelector("#stop").
+addEventListener('click' , stopChangingColor);
